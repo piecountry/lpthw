09c3643ed223aafdb3ca74a71a24690fbfbca1b3
Steve Smith <115339099+zipcochise@users.noreply.github.com>
2022-11-27 13:25:58 -0600

    11.27.22 Exercise 22 (first draft)
    I have the feeling I will keep coming back to this one.

---
+++ ex22.py
@@ -0,0 +1,73 @@
+# This is a list of Python that I've used so far.
+
+
+
+from sys import argv
+print(f" ")
+
+def fname(arg):
+    pass
+
+def mname(self, arg):
+    pass
+
+if condition:
+    pass
+
+return
+
+
+Arithmetic Operators        Name                Definition
+
+# +: plus                   Addition
+# -: minus                  Subtraction
+# /: slash                  Division
+# *: asterisk               Multiplication
+# %: percent                Modulus
+# **: double asterisk       Exponentiation
+# //: double slash          Floor division      Divide the first number by the second number and rounds the result down to the nearest integer (or whole number)
+
+Comparison Operators
+# <: less_than              Less than	x < y
+# >: greater_than           Greater than
+# <=: less_than_equal       Less than or equal to
+# >=: greater_than_equal    Greater than or equal to
+# ==: double Equal          Equal	x == y
+# !=: bang Equal            Not equal	x != y
+# >: greater than	        	x > y
+# <:
+# >=:	                    	x >= y
+# <=:	                    	x <= y
+
+Assignment Operators
+# =:	x = 5	x = 5
+# +=:	x += 3	x = x + 3
+# -=:	x -= 3	x = x - 3
+# *=:	x *= 3	x = x * 3
+# /=:	x /= 3	x = x / 3
+# %=:	x %= 3	x = x % 3
+# //=:	x //= 3	x = x // 3
+# **=:	x **= 3	x = x ** 3
+# &=:	x &= 3	x = x & 3
+# |=:	x |= 3	x = x | 3
+# ^=:	x ^= 3	x = x ^ 3
+# >>=:	x >>= 3	x = x >> 3
+# <<=:	x <<= 3	x = x << 3
+
+Escape Sequences
+
+# \\
+# \'
+# \"
+# \a
+# \b
+# \f
+# \n
+# \N{name}
+# \r
+# \t
+# \uxxxx
+# \Uxxxxxxxx
+# \v
+# \ooo
+# \xhh
