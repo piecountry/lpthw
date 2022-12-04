0a46c1fcb42d790fcda1a0aac735b0eef1efe443
Steve Smith <115339099+zipcochise@users.noreply.github.com>
2022-12-04 13:28:27 -0600

    Completed Exercise 33

---
+++ ex33.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from sys import argv
+script, threshold = argv
+
+
+def while_test(stop):
+    i = 0
+    numbers = []
+    stop = int(stop)
+
+    while i < stop:
+        print(f"At the top i is {i}")
+        numbers.append(i)
+
+        i += 1
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
+    return numbers
+
+numbers = while_test(threshold)
+
+print(f"The size of the returned list is {len(numbers)} items.")
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
