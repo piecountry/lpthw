35fe5ebd216d343946f0d315f24d6c9ef5dfb971
Steve Smith <115339099+zipcochise@users.noreply.github.com>
2022-12-09 00:10:14 -0600

    Updated Exercise 34

--- ex34.py
+++ ex34.py
@@ -1,79 +1,29 @@
 #!/usr/bin/env python3
 
-from sys import exit
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
 
-def gold_room():
-    print("This room is full of gold. How much do you take?")
+# The animal at 1.
+# 1. the first animal is at 0 and is a bear.
 
-    choice = input("> ")
-    if "0" in choice or "1" in choice:
-        how_much = int(choice)
-    else:
-        dead("Man, learn to type a number.")
+# The third (3rd) animal.
+# 2. The third annimal is at 2 and is a peacock.
 
-    if how_much < 50:
-        print("Nice, you're not greedy, you win!")
-        exit(0)
-    else:
-        dead("You greedy bastard!")
+# The first (1st) animal.
+# 3. The first animal is at 0 and is a bear.
 
+# The animal at 3.
+# 4. The third animal is a 2 and is a peacock.
 
-def bear_room():
-    print("There is a bear here.")
-    print("The bear has a bunch of honey.")
-    print("The fat bear is in front of another door.")
-    print("How are you going to move the bear?")
-    bear_moved = False #Don't use FALSE for a truth statement, it is undefined. Use False instead.
+# The fifth (5th) animal.
+# 5. The fifth animal is at 4 and is a whale.
 
-    while True:
-        choice = input("> ")
+# The animal at 2.
+# 6. The second animal is at 1 and is python3.6.
 
-        if choice == "take honey":
-            dead("The bear looks at you then slaps your face off.")
-        elif choice == "taunt bear" and not bear_moved:
-            print("The bear has moved from the door.")
-            print("You can go through it now.")
-            bear_moved = True
-        elif choice == "taunt bear" and bear_moved:
-            dead("The bear gets pissed off and chews your leg off.")
-        elif choice == "open door" and bear_moved:
-            gold_room()
-        else:
-            print("I got no idea what that means.")
+# The sixth (6th) animal.
+# 7. The sixth animal is at 5 and is a platypus.
 
+# The animal at 4.
+# 8. The 4th animal is at 3 and is a kangaroo.
 
-def cthulhu_room():
-    print("Here you see the great evil Cthulhu.")
-    print("He, it, whatever stares at you and you go insane.")
-    print("Do you flee for your life or eat your head?")
-
-    choice = input("> ")
-
-    if "flee" in choice:
-        start()
-    elif "head" in choice:
-        dead("Well that was tasty!")
-    else:
-        cthulhu_room()
-
-
-def dead(why):
-    print(why, "Good job!")
-    exit(0)
-
-def start():
-    print("You are in a dark room.")
-    print("There is a door to your right and left.")
-    print("Which one do you take?")
-
-    choice = input("> ")
-
-    if choice == "left":
-        bear_room()
-    elif choice == "right":
-        cthulhu_room()
-    else:
-        dead("You stumble around the room until you starve.")
-
-
-start()
+# A., Shaw Zed. Learn Python 3 the Hard Way (Zed Shaw's Hard Way Series) (p. 187). Pearson Education. Kindle Edition.
