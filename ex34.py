271b57461bf89323202abf2885b32fb753ff2d42
Steve Smith <115339099+zipcochise@users.noreply.github.com>
2022-12-10 12:16:04 -0600

    Updated Exercise 34

--- ex34.py
+++ ex34.py
@@ -26,4 +26,19 @@ animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
 # The animal at 4.
 # 8. The 5th animal is at 4 and is a whale.
 
+# Study Drills
+
+# 1. January 1, 2010 is a date. The order in which things happen is the ciritcal part, so dates are ordinal. That's why 1/1/2010 is in the year 2010 not 2009.
+
+# 2. study_list = ['Darwin', 'Churchill', 'Roosevelt', 'Lincoln', 'Johnson', 'Jefferson', 'Washington']
+#     The only scientist is at 0 and is Darwin.
+#     The biggest fan of America is at 1 and is Churchill.
+#     The seventh man is at 6 and is Washington.
+#     The giant at 3 is Lincoln.
+
+# 3. study_list[0] == 'Darwin',
+#     study_list[1] == 'Churchill',
+#     study_list[6] == 'Washington'
+#     study_list[3] == 'Lincoln'
+
 # A., Shaw Zed. Learn Python 3 the Hard Way (Zed Shaw's Hard Way Series) (p. 187). Pearson Education. Kindle Edition.
