dd1a45b87535a27428c152781ad44cd1e4ab8ba7
Steve Smith <115339099+zipcochise@users.noreply.github.com>
2022-12-10 09:48:12 -0600

    Updated Exercise 34

--- ex34.py
+++ ex34.py
@@ -12,18 +12,18 @@ animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
 # 3. The first animal is at 0 and is a bear.
 
 # The animal at 3.
-# 4. The third animal is a 2 and is a peacock.
+# 4. The third animal is at 3 and is a kangaroo.
 
 # The fifth (5th) animal.
 # 5. The fifth animal is at 4 and is a whale.
 
 # The animal at 2.
-# 6. The second animal is at 1 and is python3.6.
+# 6. The third animal is at 2 and is a peacock.
 
 # The sixth (6th) animal.
 # 7. The sixth animal is at 5 and is a platypus.
 
 # The animal at 4.
-# 8. The 4th animal is at 3 and is a kangaroo.
+# 8. The 5th animal is at 4 and is a whale.
 
 # A., Shaw Zed. Learn Python 3 the Hard Way (Zed Shaw's Hard Way Series) (p. 187). Pearson Education. Kindle Edition.
